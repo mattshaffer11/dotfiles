eee8265bd0e2d6033679cf5de658aaf0ad7b44f4
Matt Shaffer <mattshaffer11@gmail.com>
2022-05-26 07:04:15 -0700

    Add install script

---
+++ install.sh
@@ -0,0 +1,41 @@
+# install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install spf13-vim
+sh <(curl https://j.mp/spf13-vim3 -L)
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install Silver Searcher
+brew install the_silver_searcher
+
+# update Git version
+brew install git
+
+# install NVM
+brew install nvm
+
+# install Chrome
+brew install --cask google-chrome
+
+# install iTerm2
+brew install --cask iterm2
+
+# install Postman
+brew install --cask postman
+
+# install Webstorm
+brew install --cask webstorm
+
+# setup Hammerspoon
+mkdir -p ~/.hammerspoon
+cp .hammerspoon/init.lua ~/.hammerspoon
+
+cat << EOF
+
+Base setup complete! Other items to possibly install or setup:
+
+- Git
+- Docker
+EOF
